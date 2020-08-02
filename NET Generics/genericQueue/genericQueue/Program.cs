b25485f45b04d1411345ea4f08d7466d6485f186
Mandeep Kumar <36164725+void007@users.noreply.github.com>
2020-08-02 18:24:32 +0530

    Add files via upload

---
+++ NET Generics/genericQueue/genericQueue/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace genericQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> q = new Queue<string>();
+            q.Enqueue("Mandeep");
+            q.Enqueue("Yadav");
+            q.Enqueue("Assignment");
+            q.Enqueue("Determination");
+            q.Enqueue("Hard-work");
+            q.Enqueue("that's..it");
+
+            print(q);
+            Console.WriteLine("Retrieve using peak:");
+            Console.WriteLine(q.Peek());
+
+            Console.WriteLine("Remove two elements from the queue:");
+            q.Dequeue();
+            q.Dequeue();
+            Console.WriteLine("Updated queue is:\n");
+            print(q);
+
+
+        }
+        public static void print(Queue<string> q)
+        {
+            foreach(string name in q)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
---
+++ NET Generics/genericStack/genericStack/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace genericStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> st= new Stack<string>();
+            st.Push("Mandeep");
+            st.Push("Holiday");
+            st.Push("Determination");
+            st.Push("Assignment");
+            st.Push("logo");
+            st.Push("thats..it");
+            print(st);
+            Console.WriteLine("Top item of the stack is:");
+            Console.WriteLine(st.Peek());
+
+            Console.WriteLine("Remove top two elemens of the stack");
+            st.Pop();
+            st.Pop();
+            Console.WriteLine("Now the updated stack is:\n");
+            print(st);
+
+        }
+        public static void print(Stack<string> st)
+        {
+            foreach(string s in st)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
