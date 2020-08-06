8b1e0e5f5b38ce0d70adec88c5176c201ee81ce8
Mandeep Kumar <36164725+void007@users.noreply.github.com>
2020-08-06 23:41:59 +0530

    Add files via upload

---
+++ Net FileIO/Excercise1/Excercise1/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+
+namespace Excercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dir = @"E:\,Accolite\Net FileIO";
+            string file = @"E:\,Accolite\Net FileIO\file.txt";
+
+            if (!Directory.Exists(dir))
+            {
+                Directory.CreateDirectory(dir);
+            }
+            if (Directory.Exists(dir))
+            {
+                Console.WriteLine("Directory created");
+            }
+            //
+            if (!File.Exists(file))
+            {
+                File.CreateText(file);
+            }
+            if (File.Exists(file))
+            {
+                Console.WriteLine("text file wiithin created directory created");
+            }
+
+            //
+            Console.WriteLine("Writing into the files...");
+                using (StreamWriter writer=new StreamWriter(file))
+            {
+                writer.WriteLine("Hello, you are in first line");
+                writer.WriteLine("Second line of the file");
+                writer.WriteLine("Third line");
+                writer.WriteLine("Last line");
+
+            }
+
+            string reader = File.ReadAllText(file);
+            Console.WriteLine(reader);
+
+            Console.WriteLine("Counting number of words in file");
+            StreamReader sr = new StreamReader(file);
+            int counter = 0;
+            string delim = " ,.";
+            string[] fields = null;
+            string line = null;
+            while (!sr.EndOfStream)
+            {
+                line = sr.ReadLine();
+                line.Trim();
+                fields = line.Split(delim.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                counter += fields.Length; 
+            }
+            sr.Close();
+            Console.WriteLine(counter);
+
+            //
+            Console.WriteLine("Writing into another file in reverse order line by line");
+            string rfile = @"E:\,Accolite\Net FileIO\rfile.txt";
+            if (!File.Exists(rfile))
+            {
+                File.CreateText(rfile);
+            }
+            StreamReader objReader = new StreamReader(file);
+            string sLine = "";
+            ArrayList arrText = new ArrayList();
+
+            while (sLine != null)
+            {
+                sLine = objReader.ReadLine();
+                if (sLine != null)
+                    arrText.Add(sLine);
+            }
+            objReader.Close();
+
+
+            arrText.Reverse();
+            
+               
+                using (StreamWriter writer = new StreamWriter(rfile))
+                {
+                foreach (string str in arrText)
+                {
+                    writer.WriteLine(str);
+                    Console.WriteLine(str);
+
+                }
+
+                }
+              
+
+
+
+        }
+    }
+}
---
+++ Net FileIO/Excercise2/Excercise2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Excercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Console.WriteLine("Hello World!");
+            string bfile = @"E:\,Accolite\Net FileIO\bfile.bin";
+            try
+            {
+                
+               // BinaryWriter bw = new BinaryWriter(new FileStream(bfile, FileMode.Create));
+               // Encoding ascii = Encoding.ASCII;
+                //BinaryWriter bwEncoder = new BinaryWriter(new FileStream(bfile, FileMode.Create), ascii);
+                using (BinaryWriter bin = new BinaryWriter(File.Open(bfile, FileMode.Create)))
+                {
+                    //write into binary file
+                    bin.Write("Mandeep Kumar Yadav");
+                    bin.Write(100);
+                    bin.Write("how u doing!");
+                    bin.Write(true);
+                    bin.Write(55.55);
+                }
+                Console.WriteLine("Data Written!");
+            }
+            catch(IOException io)
+            {
+                Console.WriteLine("Error is:{0}", io.Message);
+            }
+            //
+            string ot = @"E:\,Accolite\Net FileIO\out.txt";
+
+            using(FileStream fs = File.OpenRead(bfile))
+            {
+                byte[] data = new byte[fs.Length];
+                fs.Read(data, 0, (int)fs.Length);
+
+                using (StreamWriter sw=new StreamWriter(ot))
+                {
+                    for(int i = 0; i < data.Length; i++)
+                    {
+                        if ((Convert.ToInt32(data[i]) >= 32) && (Convert.ToInt32(data[i]) <= 127) || (Convert.ToInt32(data[i]) == 10) || (Convert.ToInt32(data[i]) == 13))
+                        {
+                            sw.Write(Convert.ToChar(data[i]));
+                        }
+                    }
+                    sw.Close();
+                }
+                fs.Close();
+            }
+
+            //
+           
+
+
+        }
+    }
+}
